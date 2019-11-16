67030bf2db26d491768ac0fac6c186475c34dc43
Colin Walters <walters@verbum.org>
2019-11-16 16:48:00 +0000

    wip: separate rootfs squash

--- overlay.d/05core/usr/lib/dracut/modules.d/20live/is-live-image.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/20live/is-live-image.sh
@@ -2,4 +2,19 @@
 # Script invoked by ignition-dracut generator to detect whether this is a
 # live system without a root device.  We can't test for /run/ostree-live
 # because it may not have been created yet.
-[ -e /root.squashfs ]
+get_karg() {
+    local arg="$1"
+    local cmdline=( $(</proc/cmdline) )
+    local i
+    local k
+    local v
+    for i in "${cmdline[@]}"; do
+        k=${i%%=*};
+        if [ "$k" != "$arg" ]; then
+            continue
+        fi
+        echo ${i##*=}
+    done
+}
+
+get_karg coreos.live-root
